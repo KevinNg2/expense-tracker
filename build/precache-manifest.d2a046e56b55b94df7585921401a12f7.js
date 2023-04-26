c4b35dfc666f7dbe4a735be3d704e56a5c412858
Kevin Ng <102876076+KevinNg2@users.noreply.github.com>
2023-04-26 11:23:26 -0700

    npm run build

---
+++ build/precache-manifest.d2a046e56b55b94df7585921401a12f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eeb4f1c4e99a355fb0a3d2a079d2fda",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abf2660b74d7459bd5a6",
+    "url": "/static/css/main.708a4d9b.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf2660b74d7459bd5a6",
+    "url": "/static/js/main.1b523690.chunk.js"
+  },
+  {
+    "revision": "fd1e764c12408bbbbfc1",
+    "url": "/static/js/runtime-main.f859b388.js"
+  }
+]);
